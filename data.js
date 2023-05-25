e07247e7ffd5be6288ace683cdcc95b27f3a403e
cakenpeace <chwon02@naver.com>
2023-05-25 13:44:14 +0900

    [0525] flex 적용

--- data.js
+++ data.js
@@ -7,16 +7,17 @@ class foodData {
 }
 
 var dataArr = new Array();
-dataArr.push(new foodData("돈천동", "https://mp-seoul-image-production-s3.mangoplate.com/96788/731481_1517132364527_23832", ["일식", "돈까스"]));
-dataArr.push(new foodData("돈천동", "https://mp-seoul-image-production-s3.mangoplate.com/96788/731481_1517132364527_23832", ["일식", "돈까스"]));
-dataArr.push(new foodData("돈천동", "https://mp-seoul-image-production-s3.mangoplate.com/96788/731481_1517132364527_23832", ["일식", "돈까스"]));
+dataArr.push(new foodData("돈천동", "https://img.siksinhot.com/place/1481587042136677.jpg", ["일식", "돈까스"]));
+dataArr.push(new foodData("돈천동", "https://img.siksinhot.com/place/1481587042136677.jpg", ["일식", "돈까스"]));
+dataArr.push(new foodData("돈천동", "https://img.siksinhot.com/place/1481587042136677.jpg", ["일식", "돈까스"]));
 
+//flex 모델 적용 : 이미지만 넣을 때는 괜찮은데 글자 삽입 시 많이 고쳐야 함
 for (let i = 0; i < dataArr.length; i++) {
     var newDiv = document.createElement('div');
-    newDiv.style.cssText = 'border : 1px solid #CBCBCB; background-color : #FFF; width: 90%; height: 83px; box-shadow: 0px 1px 5px 0.5px #CBCBCB; position: relative; margin: 0 auto; margin-bottom: 10px;'; 
+    newDiv.style.cssText = 'border : 1px solid #CBCBCB; background-color : #FFF; width: 90%; height: 83px; box-shadow: 0px 1px 5px 0.5px #CBCBCB; display: flex; align-items: center; margin: 0 auto; margin-bottom: 10px;'; 
     var newImg = document.createElement('img');
     newImg.src = dataArr[i].img_link;
-    newImg.style.cssText = 'border-radius: 15%; max-height: 70px; position: relative; object-fit: cover; left: 2%; margin: 1%;';
+    newImg.style.cssText = 'border-radius: 15%; max-height: 65px; position: relative; object-fit: cover; left: 3%;';
     //var nameText = document.createTextNode(dataArr[i].name);
     //nameText.style.cssText = ''
     newDiv.appendChild(newImg);
@@ -25,6 +26,7 @@ for (let i = 0; i < dataArr.length; i++) {
     console.log("create block : " + i);
 }
 
+//수동으로 margin 적용: 화면 크기 변하면 깨짐
 for (let i = 0; i < dataArr.length; i++) {
     var newDiv = document.createElement('div');
     newDiv.style.cssText = 'border : 1px solid #CBCBCB; background-color : #FFF; width: 90%; height: 65px; box-shadow: 0px 1px 5px 0.5px #CBCBCB; position: relative; margin: 0 auto;'; 
