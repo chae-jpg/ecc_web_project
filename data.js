7962e8acbb7793848c1be4e268a35d10cec4446f
cakenpeace <chwon02@naver.com>
2023-06-20 00:26:32 +0900

    [0620] 메뉴 html에 정적으로 구현 + 하트 버튼 클릭 액션 추가

--- data.js
+++ data.js
@@ -11,13 +11,37 @@ dataArr.push(new foodData("돈천동", "https://img.siksinhot.com/place/14815870
 dataArr.push(new foodData("돈천동", "https://img.siksinhot.com/place/1481587042136677.jpg", ["일식", "돈까스"]));
 dataArr.push(new foodData("돈천동", "https://img.siksinhot.com/place/1481587042136677.jpg", ["일식", "돈까스"]));
 
-//flex 모델 적용 : 이미지만 넣을 때는 괜찮은데 글자 삽입 시 많이 고쳐야 함
+
+
+
+//flex 모델 적용
 for (let i = 0; i < dataArr.length; i++) {
     var newDiv = document.createElement('div');
-    newDiv.style.cssText = 'border : 1px solid #CBCBCB; background-color : #FFF; width: 90%; height: 83px; box-shadow: 0px 1px 5px 0.5px #CBCBCB; display: flex; align-items: center; margin: 0 auto; margin-bottom: 10px;'; 
+    newDiv.style.cssText = 'border : 1px solid #CBCBCB; background-color : #FFF; width: 80%; height: 83px; box-shadow: 0px 1px 5px 0.5px #CBCBCB; display: flex; align-items: center; margin: 0 auto; margin-bottom: 10px; border: none;'; 
     var newImg = document.createElement('img');
     newImg.src = dataArr[i].img_link;
-    newImg.style.cssText = 'border-radius: 15%; max-height: 65px; position: relative; object-fit: cover; left: 3%;';
+    newImg.style.cssText = 'border-radius: 15%; max-height: 60px; position: relative; object-fit: cover; left: 8%;';
+    var nameText = document.createElement('span');
+    var nameTextText = document.createTextNode(dataArr[i].name);
+    nameText.append(nameTextText);
+    nameText.style.cssText= 'align-self: flex-start; margin-left: 15%; margin-top: 10px; font-weight: bold; font-size: 1rem;';
+    var heartBtn = document.createElement('button');
+    heartBtn.style.cssText= 'background: url("heart.png"); background-size: cover; height: 20px; width: 20px; margin-left: auto; align-self: flex-start; margin-top: 10px; border: none; margin-right: 20px;'
+    heartBtn.className += "heartBtn";
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(nameText);
+    newDiv.appendChild(heartBtn);
+    document.body.appendChild(newDiv);
+    console.log("create block : " + i);
+
+}
+
+for (let i = 0; i < dataArr.length; i++) {
+    var newDiv = document.createElement('div');
+    newDiv.style.cssText = 'border : 1px solid #CBCBCB; background-color : #FFF; width: 80%; height: 65px; box-shadow: 0px 1px 5px 0.5px #CBCBCB; display: flex; align-items: center; margin: 0 auto; border: none;'; 
+    var newImg = document.createElement('img');
+    newImg.src = dataArr[i].img_link;
+    newImg.style.cssText = 'border-radius: 15%; max-height: 50px; position: relative; object-fit: cover; left: 4%; margin: 1%;';
     //var nameText = document.createTextNode(dataArr[i].name);
     //nameText.style.cssText = ''
     newDiv.appendChild(newImg);
@@ -26,17 +50,6 @@ for (let i = 0; i < dataArr.length; i++) {
     console.log("create block : " + i);
 }
 
-//수동으로 margin 적용: 화면 크기 변하면 깨짐
-for (let i = 0; i < dataArr.length; i++) {
-    var newDiv = document.createElement('div');
-    newDiv.style.cssText = 'border : 1px solid #CBCBCB; background-color : #FFF; width: 90%; height: 65px; box-shadow: 0px 1px 5px 0.5px #CBCBCB; position: relative; margin: 0 auto;'; 
-    var newImg = document.createElement('img');
-    newImg.src = dataArr[i].img_link;
-    newImg.style.cssText = 'border-radius: 15%; max-height: 55px; position: relative; object-fit: cover; left: 2%; margin: 1%;';
-    //var nameText = document.createTextNode(dataArr[i].name);
-    //nameText.style.cssText = ''
-    newDiv.appendChild(newImg);
-    //newDiv.appendChild(nameText);
-    document.body.appendChild(newDiv);
-    console.log("create block : " + i);
-}
+document.getElementsByClassName("heartBtn").onclick = function () {
+    document.getElementsByClassName("heartBtn").style.backgroundImage = "url('home.png')";
+};
---
+++ menu.js
@@ -0,0 +1,20 @@
+
+const heart_btn = document.querySelectorAll('.heart_btn');
+for (let i = 0; i < heart_btn.length; i++) {
+    heart_btn[i].addEventListener("click", function () {
+        console.log("button clicked");
+        if (heart_btn[i].classList.contains("not_clicked")) {
+            console.log("empty button clicked");
+            heart_btn[i].style.background = "url('heart_filled.png')";
+            heart_btn[i].style.backgroundSize = "cover";
+            heart_btn[i].classList.remove("not_clicked");
+            heart_btn[i].classList.add("clicked");
+        } else {
+            console.log("filled button clicked");
+            heart_btn[i].style.background = "url('heart_empty.png')";
+            heart_btn[i].style.backgroundSize = "cover";
+            heart_btn[i].classList.remove("clicked");
+            heart_btn[i].classList.add("not_clicked");
+        }
+    });
+}
